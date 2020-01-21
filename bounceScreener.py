9206c59e3e7064f9796aa3b72921e00ac2de28b0
bALarkA <34934124+panda-balarka@users.noreply.github.com>
2020-01-22 00:03:52 +0530

    DraftWorking_Screener [Improve Parameters for Hits]

--- bounceScreener.py
+++ bounceScreener.py
@@ -6,13 +6,14 @@ Created on Mon Jan 20 22:50:10 2020
 """
 
 from nselist import NSE_TradedStocks
-from auxFuncs import convertDate,getDate_previous,getDate_today,getDate_yesterday
+from auxFuncs import getDate_previous,getDate_today,getDate_yesterday,printProgressBar
 from instrument_request import INSTRUMENT_DATA
 from technicalWrapper import TECH_FXS
-from screenAlgo import isEMA_Long
+from screenAlgo import BOUNCESCREENER
 
-def screenStocks(volumeCutoff=150000,position='long',bounce18=False,bounce50=True,bounce100=False):
-    stocksList = NSE_TradedStocks(convertDate('20-01-2020'))
+def screenStocks(volumeCutoff=150000,position='long',bounce18=False,bounce50=True,bounce100=False,endDate_select='Today'):
+    endDate = getDate_today() if endDate_select == 'Today' else getDate_yesterday()
+    stocksList = NSE_TradedStocks(endDate)
     screendedInstruments = {
         '18 Bounce Long'  : [],
         '50 Bounce Long'  : [],
@@ -21,11 +22,11 @@ def screenStocks(volumeCutoff=150000,position='long',bounce18=False,bounce50=Tru
     # ensure this value is yesterday if the script is run in the morning before trading session
     # or should be today if the analysis is done after the close of the market for the present
     # day
-    endDate = getDate_yesterday()
-    startDate = getDate_previous(500,endDate)
+    startDate = getDate_previous(3650,endDate)
 
-    stocksList = stocksList[:5]
-    for currentInstrument in stocksList:
+    stocksList = stocksList[:100]
+    printProgressBar(0,len(stocksList),prefix='Progress:', suffix = 'Complete', length = 50)
+    for idx,currentInstrument in enumerate(stocksList):
         # fetch histrorical data of required equity instrument
         temp_obj = INSTRUMENT_DATA(currentInstrument)
         temp_obj.requestData(startDate,endDate)
@@ -37,19 +38,24 @@ def screenStocks(volumeCutoff=150000,position='long',bounce18=False,bounce50=Tru
             # get the required technical data using the talib interfaces to perform the bounce screening
             # of the instruments        
             technical_obj = TECH_FXS(primaryData)
+            # EMAs are Pandas Series type
             EMA18 = technical_obj.getEMA(18)
             EMA50 = technical_obj.getEMA(50)
             EMA100 = technical_obj.getEMA(100)
-            EMA200 = technical_obj.getEMA(200)
+            EMA200 = technical_obj.getSMA(200)
+            # As the EMA values are not accurate for longer time periods, we use 
+            # SMA as substitute and manually verify the same for the screened 
+            # instruments
+            # MACD and STOCHASTICS are both Pandas Dataframe type
             MACD50 = technical_obj.getMACD(50,100,9)
             MACD18 = technical_obj.getMACD(18,50,9)
             STOCH5_33 = technical_obj.getSTOCH(5,3,3)
             del technical_obj
 
             if position == 'long':
-                EMA_check = 
                 # create the object for long position screening
                 bounceScreener_obj = BOUNCESCREENER(primaryData,EMA18,EMA50,EMA100,EMA200,MACD18,MACD50,STOCH5_33)
+                bounceScreener_obj.longScreener_initParams()
 
                 if bounce18:
                     if bounceScreener_obj.isInstrument_bounce18longMatch(): screendedInstruments['18 Bounce Long'].append(currentInstrument)
@@ -64,6 +70,19 @@ def screenStocks(volumeCutoff=150000,position='long',bounce18=False,bounce50=Tru
                 # code the short position calculations
                 pass 
                 print("Short Position Scans still not supported")
+        printProgressBar(idx+1,len(stocksList),prefix='Progress:', suffix = 'Complete', length = 50)
+                
+    if (
+            len(screendedInstruments['18 Bounce Long']) > 0
+        or  len(screendedInstruments['50 Bounce Long']) > 0
+        or  len(screendedInstruments['100 Bounce Long']) > 0
+        ):
+        print(screendedInstruments)
+    else:
+        print('No Instrument matching screening criteria found')
+                
+if __name__ == '__main__':
+    screenStocks(100000,bounce18=True,bounce50=True,bounce100=True)
 
 
 
--- screenAlgo.py
+++ screenAlgo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 20 22:50:10 2020
+TODO: Many check function boundary conditions with respect to list lengths missing.
 
 @author: Balarka
 """
@@ -12,50 +13,160 @@ class BOUNCESCREENER(object):
         self.high = dataFrame['high'].tolist()
         self.close = dataFrame['close'].tolist()
         self.low = dataFrame['low'].tolist()
-        self.EMA18 = EMA18['EMA18'].tolist()
-        self.EMA50 = EMA18['EMA18'].tolist()
-        self.EMA100 = EMA18['EMA18'].tolist()
-        self.EMA200 = EMA18['EMA18'].tolist()
-        self.MACD18 = MACD18
-        self.MACD50 = MACD50
+        self.EMA18 = EMA18.tolist()
+        self.EMA50 = EMA50.tolist()
+        self.EMA100 = EMA100.tolist()
+        self.EMA200 = EMA200.tolist()
+        self.MACD = { '18' : MACD18,
+                      '50' : MACD50
+                      }
         self.STOCH = STOCH5_33
+        
+    def longScreener_initParams(self,stochasticThreshold=30,stochasticTPeriod=5):
+        
+        self.isEMA_StateLong = self.isEMA_Long(4)
+        self.isStochastics_OverSold = self.isSTOCH_OverSold(stochasticThreshold,stochasticTPeriod)
+        # used specifically for 18 bounce long
+        self.MACD18_isPos = self.MACD_neg2pos('18',1)
+        self.MACD50_isPos = self.MACD_neg2pos('50',1)
+        # use for 50,100 bounce long
+        self.MACD50_Ok = self.isMACD_BullBearCross_OK('50',5)
+        
 
-    def isEMA_Long(self,period=10):
-        tempValue_list = [self.EMA18,self.EMA50,self.EMA100,self.EMA200]
+    def isEMA_Long(self,period=5):
+        tempValue_list = [self.EMA18,self.EMA50,self.EMA100]
+        # EMA200 Calculations are not so accurate, so ignored until an alternative solution can be implemented
+        # tempValue_list = [self.EMA18,self.EMA50,self.EMA100,self.EMA200]
         for emaListIdx in range(len(tempValue_list)-1):
-            for value in range(len(period)):
+            for value in range(period):
                 if tempValue_list[emaListIdx][-value-1] < tempValue_list[emaListIdx+1][-value-1]:
-                    # if EMAa value falls below EMAb then the EMA has become negetive 
+                    # if EMAa value falls below EMAb then the EMA suggests short position
+                    # so return False
                     return False
         return True
-
+    
+    def isSTOCH_OverSold(self,threshold=30,tracePeriod=3):
+        slowK_list = self.STOCH['slowk'].tolist()
+        for traceIdx in range(tracePeriod):
+            if slowK_list[-traceIdx-1] <= threshold:
+                return True
+        return False
+    
+    def MACD_neg2pos(self,MACDtype,tracePeriod):
+        # if macd has crossed signal line and is positive, the macdhist value will be positive
+        macdHist = self.MACD[MACDtype]['macdhist'].tolist()
+        for traceIdx in range(tracePeriod):
+            if macdHist[-traceIdx-1] < 0:
+                return False
+        return True
+    
+    def isMACD_BullBearCross_OK(self,MACDtype,tracePeriod):
+        # macdhist provides the difference between macd and macdSignal, can be used to check when
+        # the macd went bearish
+        macdHist = self.MACD[MACDtype]['macdhist'].tolist()     
+        # if MACD is already positive, then no further checks
+        if macdHist[-1] > 0:
+            return True
+        # counter to check when the MACD went from Pos to Neg value
+        posCtr = 0
+        # loop and calculate how long the MACD has been negetive
+        for traceIdx in range(len(macdHist)):
+            if macdHist[-traceIdx-1] < 0:
+                posCtr += 1
+            else:
+                break
+        if posCtr >= tracePeriod:
+            return True
+        else:
+            return False
+    
+    def isCandle_BullishReversal(self,patternType,emaType):
+        # each of the input parameter is a list of 2 or 3 values, 
+        # if two : first element has the reversal candle info and the second has the confirmation candle info. 
+        # This is used to checks if its a EMA bounce with Single Candle reversal or two candle reversal with original 
+        # and inside bar pattern
+        # if three : first element has the Bearish Candle info, the second element has the reversal candle info and 
+        # the last element has the confirmation candle info. This function checks if its a EMA bounce with two candle 
+        # trade through pattern
+        emaDict = {
+                    '18' : self.EMA18,
+                    '50' : self.EMA50,
+                    '100' : self.EMA100,
+                    }
+        openVals = self.open[-patternType:]
+        highVals = self.high[-patternType:]
+        closeVals = self.close[-patternType:]
+        lowVals = self.low[-patternType:]
+        emaVals = emaDict[emaType][-patternType:]
+        if patternType == 2:
+            rvrsCandle  = 0
+            cnfrmCandle = 1
+            # The below conditions expand in simple term to the reversal candle's shadow cutting the EMA line and
+            # the confirmation candle being bullish and closing above the reversal candle and the other conditions
+            # for a long position
+            if ( 
+                    (lowVals[rvrsCandle] < emaVals[rvrsCandle])
+                and (openVals[rvrsCandle] > emaVals[rvrsCandle])
+                and (closeVals[rvrsCandle] > emaVals[rvrsCandle])
+                and (closeVals[cnfrmCandle] > highVals[rvrsCandle])
+                and (closeVals[cnfrmCandle] > closeVals[cnfrmCandle])
+                ):
+                return True
+            else:
+                return False
+        elif patternType == 3:
+            bearCandle = 0
+            rvrsCandle = 1
+            cnfrmCandle = 2
+            # The below conditions expand in simple term to a bearish and bullish candle cutting through an EMA line
+            # a confirmation bullish candle closing above the high of the bullish reversal candle
+            if (
+                    (openVals[bearCandle] > emaVals[bearCandle])
+                and (closeVals[bearCandle] < emaVals[bearCandle])
+                and (openVals[rvrsCandle] < emaVals[rvrsCandle])
+                and (closeVals[rvrsCandle] > emaVals[rvrsCandle])
+                and (closeVals[cnfrmCandle] > openVals[cnfrmCandle])
+                and (closeVals[cnfrmCandle] > highVals[rvrsCandle])
+                ):
+                return True
+            else:
+                return False            
+        
+                
     def isInstrument_bounce18longMatch(self):
-        if (
-            (isEMA_Long) and
-            (self.low[-2] < self.EMA18[-2]) and
-            (MACD_pos2neg('18',12)) and
-            (MACD_pos2neg('50',12)) and
-            (STOCH_oversold(30,3)) and
-            (self.open[-2] > self.EMA18[-2]) and
-            (self.close[-2] > self.EMA18[-2]) and
-            (self.close[-1] > self.high[-2]) and
-            (self.close[-1] > self.open[-1]) and
+        if (    
+                (self.isEMA_StateLong)
+            and (self.isStochastics_OverSold)
+            and (self.MACD18_isPos)
+            and (self.MACD50_isPos)
+            and (self.isCandle_BullishReversal(2,'18') or self.isCandle_BullishReversal(3,'18'))
             ):
             return True
         else:
             return False
 
     def isInstrument_bounce50longMatch(self):
-        pass
+        if (    
+                (self.isEMA_StateLong)
+            and (self.isStochastics_OverSold)
+            and (self.MACD50_Ok)
+            and (self.isCandle_BullishReversal(2,'50') or self.isCandle_BullishReversal(3,'50'))
+            ):
+            return True
+        else:
+            return False
 
     def isInstrument_bounce100longMatch(self):
-        pass
+        if (    
+                (self.isEMA_StateLong)
+            and (self.isStochastics_OverSold)
+            and (self.MACD50_Ok)
+            and (self.isCandle_BullishReversal(2,'100') or self.isCandle_BullishReversal(3,'100'))
+            ):
+            return True
+        else:
+            return False
 
-    def MACD_pos2neg(self,MACDtype,period):
-        pass
-
-    def STOCH_oversold(self,threshold,period):
-        pass
     
 
             
