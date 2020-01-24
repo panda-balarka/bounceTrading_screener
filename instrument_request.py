5300d2bc16579b7a217ddbda92535160d2511e58
panda_balarka <spandan.mahadevegowda@in.bosch.com>
2020-01-24 11:53:11 +0530

    Support to fetch stock information from Yahoo

--- instrument_request.py
+++ instrument_request.py
@@ -6,20 +6,27 @@ Created on Mon Jan 20 20:23:37 2020
 """
 
 import nsepy
+import pandas_datareader.data as web
+import requests
 
 class INSTRUMENT_DATA(object):
     
-    def __init__(self,symbol):
+    def __init__(self,symbol,API='NSE'):
         # initialises an object variable to store the SCRIP SYMBOL
         # used to fetch data from NSE
         self.symbol = symbol
+        self.API = API
     
-    def requestData(self,startDate,endDate):
-        # call the nsepy wrapper to fetch historical data of the
-        # requested symbol from the NSE website
-        self.instrument_data = nsepy.get_history(self.symbol,
-                                           start=startDate,
-                                           end=endDate)
+    def requestData(self,startDate,endDate,customSession=None):
+        if self.API == 'NSE':
+            # call the nsepy wrapper to fetch historical data of the
+            # requested symbol from the NSE website
+            self.instrument_data = nsepy.get_history(self.symbol,
+                                               start=startDate,
+                                               end=endDate)
+        elif self.API == 'YAHOO':
+            self.instrument_data = web.DataReader(self.symbol, 'yahoo', startDate, endDate, session=customSession)
+            
         self.instrument_data.rename(columns={'Open':'open',
                                              'High':'high',
                                              'Low':'low',
@@ -41,12 +48,38 @@ class INSTRUMENT_DATA(object):
 # Script standalone selftest
 if __name__ == '__main__':
     
-    # Load data of equity from SBIN
-    temp_obj = INSTRUMENT_DATA('SBIN')
-    # Fetch the values from
-    temp_obj.requestData([1,12,2019],[20,1,2020])
-    # Display the dataframe with the required results
-    print(temp_obj.get_primeData())
+    from auxFuncs import convertDate
+    
+    apiType = "YAHOO"
+    if apiType == "NSE":
+        # Load data of equity from SBIN
+        temp_obj = INSTRUMENT_DATA('SBIN')
+        # Fetch the values from
+        temp_obj.requestData(convertDate('01/01/2019'),convertDate('01/01/2020'))
+        # Display the dataframe with the required results
+        print(temp_obj.get_primeData().tail())
+        
+    elif apiType == 'YAHOO':
+        proxies = {}
+        headers = {     "Accept":"application/json",
+                    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                    "Accept-Encoding":"none",
+                    "Accept-Language":"en-US,en;q = 0.8",
+                    "Connection":"keep-alive",
+                    "Referer":"https://cssspritegenerator.com",
+                    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+                    }
+
+        with requests.Session() as sess:
+            sess.headers = headers
+            sess.proxies.update(proxies)
+            
+        # Load data of equity from SBIN
+        temp_obj = INSTRUMENT_DATA('SBIN.NS',apiType)    
+        # Fetch the values from
+        temp_obj.requestData(convertDate('01/01/2019'),convertDate('23/01/2020'),customSession=sess) 
+        # Display the dataframe with the required results
+        print(temp_obj.get_primeData().tail())
     
     
     
